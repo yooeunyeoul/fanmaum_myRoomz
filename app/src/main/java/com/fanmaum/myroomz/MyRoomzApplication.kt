1172a642eb01b9052c2e5055662b72e5a55ef938
choiyh2709 <yh.choi@fanmaum.com>
2021-11-11 09:03:33 +0900

    패키지 분리

---
+++ app/src/main/java/com/fanmaum/myroomz/MyRoomzApplication.kt
@@ -0,0 +1,9 @@
+package com.fanmaum.myroomz
+
+import androidx.multidex.MultiDexApplication
+
+class MyRoomzApplication : MultiDexApplication() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}

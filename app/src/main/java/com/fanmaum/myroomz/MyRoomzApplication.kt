6b9b0f87ce76a8f89895cdcb39a8da79c432aec3
choiyh2709 <yh.choi@fanmaum.com>
2021-11-11 10:12:17 +0900

    dagger hilt merge

--- app/src/main/java/com/fanmaum/myroomz/MyRoomzApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.fanmaum.myroomz
-
-import androidx.multidex.MultiDexApplication
-
-class MyRoomzApplication : MultiDexApplication() {
-    override fun onCreate() {
-        super.onCreate()
-    }
-}
